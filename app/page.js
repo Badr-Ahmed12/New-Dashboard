06fe679e3a7542cf72461d997837557e62b3c595
Badr-Ahmed12 <isob83709@gmail.com>
2025-08-22 22:45:52 +0300

    add logo

--- app/page.js
+++ app/page.js
@@ -1,12 +1,12 @@
 import Cards from "@/components/Cards";
 import DashboardCharts from "@/components/Dashboard";
-
 import Layout from "@/components/Layout";
 import OverviewCard from "@/components/Overview_Card";
 
 export default function Home() {
   return (
   <>  
+
   <Layout>
     <h1 className="text-4xl font-bold mb-6 font-sans">Dashboard</h1>
     <Cards />
